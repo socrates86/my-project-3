6dc645f29173350343c79883eec6297f9bdb0fc1
socrates86 <morrisonikechukwu86@gmail.com>
2023-03-06 12:47:08 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import Info from './Info';
+import About from './About';
+import Interest from './Interest';
+import Footer from './Footer';
+import "./Info.css";
+import "./About.css";
+import "./Interest.css"
+import "./footer.css"
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-lg p-1 my-2 mb-4 border'>
+      <Info />
+      <About />
+      <Interest />
+      <Footer />
     </div>
-  );
-}
-
-export default App;
+  )
+} 
